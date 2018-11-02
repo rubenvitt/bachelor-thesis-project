14f5614f7ef7035914efeb09bb97e2b18a596a2c
rubeen <rubeen@rubeen.me>
2018-11-02 17:04:44 +0100

    implementation of 404 error

--- frontend-server/app.js
+++ frontend-server/app.js
@@ -28,4 +28,19 @@ app.set('views', __dirname + "/public");
 app.engine("html", engines.mustache);
 app.set("view engine", "html");
 
+app.use(function (req, res, next) {
+    res.status(404);
+
+    if (req.accepts('html')) {
+        res.render('404', {url: req.url});
+        return;
+    }
+    if (req.accepts('json')) {
+        res.send({error: 'Not found'});
+        return;
+    }
+    res.type('txt').send('Not found.');
+});
+
+
 module.exports = app;
