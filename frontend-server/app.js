95e4d5c08897f6a6c51fcb9d61cef2a53d822df5
rubeen <rubeen@rubeen.me>
2018-11-02 16:55:35 +0100

    add demo auth

--- frontend-server/app.js
+++ frontend-server/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
+var engines = require('consolidate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,11 +12,20 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: '4DAxTM<G2A<*|1FXS_M}8fuOj4+E`LC64~|Liu"VT&+*zGKg,c-;1}z<L{&&Aw0',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.set('views', __dirname + "/public");
+app.engine("html", engines.mustache);
+app.set("view engine", "html");
 
 module.exports = app;
--- frontend-server/routes/index.js
+++ frontend-server/routes/index.js
@@ -1,9 +1,54 @@
 var express = require('express');
+var debug = require('debug')('frontend-server:server');
+var crypto = require('crypto');
 var router = express.Router();
 
+var sessions = {};
+sessions.keys = [];
+const cookieNamePrivateKey = 'PRIVATE-TOKEN';
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    if (!showLoginIfNecessary(req)) {
+        debug("I know you!");
+        res.render('index');
+    } else {
+        debug("You are new!");
+        var entry = {key: req.session.id, val: crypto.randomBytes(64).toString('hex')};
+        sessions.keys.push(entry);
+        res.cookie(cookieNamePrivateKey, entry.val, {httpOnly: true});
+        res.render("signin");
+    }
 });
+
+router.get('/abc', function (req, res) {
+    res.render("forms");
+});
+
+router.get("/test", function (req, res) {
+    //res.sendFile(path.join(__dirname, '../public/signin.html'));
+    res.render('signin');
+});
+
+function showLoginIfNecessary(req) {
+    debug("sessionID: " + req.session.id);
+    //read cookie
+    const cookieValue = req.cookies[cookieNamePrivateKey];
+    debug("Read cookie: " + cookieValue);
+
+    //get list and check, if session exist already...
+    let keyValuePair;
+    sessions.keys.forEach(value => keyValuePair = value.key === req.session.id ? value : keyValuePair);
+    if (keyValuePair === undefined)
+        debug("It is a new session!");
+    else
+        debug("It is not a new session!");
+
+    //create a null-object for session
+    keyValuePair = keyValuePair === undefined ? {key: "abc", val: 11} : keyValuePair;
+    debug("key: " + keyValuePair.key + " contains: " + keyValuePair.val + " === " + cookieValue + "?");
+    //return if user is logged in...
+    return keyValuePair.val !== cookieValue;
+}
 
 module.exports = router;
